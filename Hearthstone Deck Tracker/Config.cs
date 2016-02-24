e3455b1838ee21f702721b7a53b43c6d5abb74b1
Alexander Zeier <epikz37@gmail.com>
2016-02-24 11:08:14 +0100

    hsreplay: implement autoupload option

--- Hearthstone Deck Tracker/Config.cs
+++ Hearthstone Deck Tracker/Config.cs
@@ -403,6 +403,9 @@ namespace Hearthstone_Deck_Tracker
 		[DefaultValue(true)]
 		public bool HighlightLastDrawn = true;
 
+		[DefaultValue(true)]
+		public bool HsReplayAutoUpload = true;
+
 		[DefaultValue(-1)]
 		public int IgnoreNewsId = -1;
 
